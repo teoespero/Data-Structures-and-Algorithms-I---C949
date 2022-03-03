436096e8bda35e21a9d37020abbed917d96b07f9
Teo Espero <38012835+teoespero@users.noreply.github.com>
2022-03-02 23:17:14 -0800

    Add files via upload

---
+++ BubbleSort/BubbleSort.cpp
@@ -0,0 +1,49 @@
+////////////////////////////////////////////////////////////////////////////////
+//  BubbleSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//  Teodulfo Espero
+//  BS Cloud and Systems Administration
+//  BS Software Development
+//  Western Governors University
+////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <array>
+using namespace std;
+
+
+////////////////////////////////////////////////////////////////////////////////
+// bubble sort function implementation
+int* bubble_sort(int toBeSorted[], int listSize) {
+	int unsorted_until_index = listSize - 1;
+	int temp=0;
+	bool sorted = false;
+
+	while (!sorted) {
+		sorted = true;
+		for (int i = 0; i < unsorted_until_index; i++) {
+			if (toBeSorted[i] > toBeSorted[i + 1]) {
+				temp = toBeSorted[i];
+				toBeSorted[i] = toBeSorted[i + 1];
+				toBeSorted[i + 1] = temp;
+				sorted = false;
+			}
+		}
+	}
+	return toBeSorted;
+
+}
+
+int main(){
+	int myArray[] = {3,2,4,1,8,5,6,9,7};
+	int len = (sizeof(myArray) / sizeof(myArray[0]));
+
+	int* myPtr = bubble_sort(myArray, len);
+
+	for (int i = 0; i < 9; ++i) {
+		cout << myPtr[i];
+		if (i < len -1) 
+			cout << ", ";
+	}
+
+}
+

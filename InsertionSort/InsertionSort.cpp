b7d2aa5f4940c5a3a66ff200db29b67dd8f43201
Teo Espero <38012835+teoespero@users.noreply.github.com>
2022-03-03 22:42:24 -0800

    Add files via upload

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -23,6 +23,8 @@ void print(int* theArray, int theSize) {
 ////////////////////////////////////////////////////////////////////////////////
 // insertion sort function implementation
 int* insertionSort(int arrayToSort[], int arraySize) {
+
+	// declare and initialize our variables
 	int
 		position = 0,
 		tempValue = 0,
@@ -31,36 +33,73 @@ int* insertionSort(int arrayToSort[], int arraySize) {
 		shifts = 0,
 		totComparison = 0;
 
+
+	// Each round of this loop represents a pass-through
 	for (int index = 1; index < arraySize; index++) {
+		
+		// we save the value we’re “removing” in a variable
+		// called temp_value
 		tempValue = arrayToSort[index];
+
+		// position will represent each value we
+		// compare against the temp_value
+		// this position will keep moving leftward
+		// as we compare each value to the temp_value
 		position = index - 1;
+		
+		// get the stats for our program
+		// the steps, the passes, and comparison count
 		comparisonCtr += 1;
 		totComparison += comparisonCtr;
 		++steps;
 
+		// print the current state of the array
+		// for every pass
 		cout << "Pass # " << steps << " - ";
 		print(arrayToSort, arraySize);
 		cout << " -> " << comparisonCtr << " comparisons, " << shifts << " shifts " << endl;
+
+		// h runs as long as position is greater or
+		// equal to 0
 		while (position >= 0) {
+
+			// check whether the value at position is greater 
+			// the temp value
 			if (arrayToSort[position] > tempValue) {
+
+				// if it is, then shift the values to the right
 				arrayToSort[position + 1] = arrayToSort[position];
+
+				// decrement position by 1 to compare the next 
+				// left value against the temp_value
 				position--;
+
+				// we also need to know how many shifts
+				// are done
 				shifts += 1;
 			}
 			else {
 				break;
 			}
+
+			// the final step of each pass-through is moving 
+			// the temp_value into the gap
 			arrayToSort[position + 1] = tempValue;
 
 		}
 		
 		
 	}
+
+	// print out our stats
 	cout << endl
 		<< "Passes: " << steps << endl
 		<< "Comparisons: " << totComparison << endl
 		<< "Shifts: " << shifts << endl;
 	
+
+	// After all pass-throughs have been completed, 
+	// we return the sorted array
 	return arrayToSort;
 }
 
@@ -90,3 +129,5 @@ int main(){
 
 	return 0;
 }
+////////////////////////////////////////////////////////////////////////////////
+// terminate and exit
